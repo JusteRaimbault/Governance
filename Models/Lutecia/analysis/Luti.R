0f6231ed6749672560440e62cd8c329090a6f3d8
Juste Raimbault <juste.raimbault@polytechnique.edu>
2018-04-19 16:34:19 +0200

    results_direct_sampling

--- Models/Lutecia/analysis/Luti.R
+++ Models/Lutecia/analysis/Luti.R
@@ -6,7 +6,11 @@ library(ggplot2)
 
 source(paste0(Sys.getenv('CS_HOME'),'/CityNetwork/Models/Utils/R/plots.R'))
 
-res <- as.tbl(read.csv('exploration/20180417_184334_SPACEMATTERS.csv'))
+resfile='20180417_184334_SPACEMATTERS'
+res <- as.tbl(read.csv(paste0('exploration/',resfile,'.csv')))
+
+resdir = paste0(Sys.getenv('CS_HOME'),'/Governance/Results/Luti/',resfile,'/');dir.create(resdir)
+
 
 # counts
 res %>% group_by(id,scenario) %>% summarize(count=n())
@@ -22,34 +26,46 @@ res %>% group_by(parid) %>% summarize(scenarios=length(unique(scenario)),count=n
 
 
 # histograms
-g = ggplot(res[res$parid==1,],aes(x=relativeAccessibility,group=paste0(parid,"-",scenario),colour=paste0(parid,"-",scenario)))
-g+geom_density(alpha=0.4)
+#g = ggplot(res[res$parid==1,],aes(x=relativeAccessibility,group=paste0(parid,"-",scenario),colour=paste0(parid,"-",scenario)))
+#g+geom_density(alpha=0.4)
 
 # test rough confidence intervals with estimated sigma
-sres = res %>% group_by(id,scenario) %>% summarize(parid= parid[1] ,avg = mean(relativeAccessibility),ci = sd(relativeAccessibility)*1.96/sqrt(n()))
+#sres = res %>% group_by(parid,scenario) %>% summarize(avgRelativeAccessibility = mean(relativeAccessibility),ci = sd(relativeAccessibility)*1.96/sqrt(n()))
+#g = ggplot(sres[sres$parid%in%1:3,])
+#g+geom_vline(aes(xintercept=avgRelativeAccessibility,colour=paste0(parid,"-",scenario)))+geom_vline(aes(xintercept=avgRelativeAccessibility-ci,colour=paste0(parid,"-",scenario)),linetype=2)+geom_vline(aes(xintercept=avgRelativeAccessibility+ci,colour=paste0(parid,"-",scenario)),linetype=2)
 
-g = ggplot(sres[sres$id%in%1:10,])
-g+geom_vline(aes(xintercept=avg,colour=paste0(id,scenario)))+geom_vline(aes(xintercept=avg-ci,colour=paste0(id,scenario)),linetype=2)+geom_vline(aes(xintercept=avg+ci,colour=paste0(id,scenario)),linetype=2)
+#full plots : distributions and averages
 
-#full plots
+parids=c(10,20)
 
-parids=c(1,2)
-
-sres = res %>% group_by(id,scenario) %>% summarize(parid= parid[1] ,avg = mean(relativeAccessibility),ci = sd(relativeAccessibility)*1.96/sqrt(n()))
+sres = res %>% group_by(parid,scenario) %>% summarize(avg = mean(relativeAccessibility),ci = sd(relativeAccessibility)*1.96/sqrt(n()))
 g = ggplot(res[res$parid%in%parids,],aes(x=relativeAccessibility,group=paste0(parid,"-",scenario),colour=paste0(parid,"-",scenario)))
-g+geom_density(alpha=0.4)+geom_vline(data=sres[sres$parid%in%parids,],aes(xintercept=avg,colour=paste0(parid,"-",scenario)))+geom_vline(data=sres[sres$parid%in%parids,],aes(xintercept=avg-ci,colour=paste0(parid,"-",scenario)),linetype=2)+geom_vline(data=sres[sres$parid%in%parids,],aes(xintercept=avg+ci,colour=paste0(parid,"-",scenario)),linetype=2)
+g+geom_density(alpha=0.4)+geom_vline(data=sres[sres$parid%in%parids,],aes(xintercept=avg,colour=paste0(parid,"-",scenario)))+geom_vline(data=sres[sres$parid%in%parids,],aes(xintercept=avg-ci,colour=paste0(parid,"-",scenario)),linetype=2)+geom_vline(data=sres[sres$parid%in%parids,],aes(xintercept=avg+ci,colour=paste0(parid,"-",scenario)),linetype=2)+
+  scale_color_discrete(name="id-scenario")+xlab(expression(tilde(X)))+stdtheme
+ggsave(filename = paste0(resdir,'distrib_relativeAccessibility.png'),width=15,height=10,units='cm')
 
-sres = res %>% group_by(id,scenario) %>% summarize(parid= parid[1] ,avg = mean(nwBetweenness),ci = sd(nwBetweenness)*1.96/sqrt(n()))
+sres = res %>% group_by(parid,scenario) %>% summarize(avg = mean(nwBetweenness),ci = sd(nwBetweenness)*1.96/sqrt(n()))
 g = ggplot(res[res$parid%in%parids,],aes(x=nwBetweenness,group=paste0(parid,"-",scenario),colour=paste0(parid,"-",scenario)))
-g+geom_density(alpha=0.4)+geom_vline(data=sres[sres$parid%in%parids,],aes(xintercept=avg,colour=paste0(parid,"-",scenario)))+geom_vline(data=sres[sres$parid%in%parids,],aes(xintercept=avg-ci,colour=paste0(parid,"-",scenario)),linetype=2)+geom_vline(data=sres[sres$parid%in%parids,],aes(xintercept=avg+ci,colour=paste0(parid,"-",scenario)),linetype=2)
+g+geom_density(alpha=0.4)+geom_vline(data=sres[sres$parid%in%parids,],aes(xintercept=avg,colour=paste0(parid,"-",scenario)))+geom_vline(data=sres[sres$parid%in%parids,],aes(xintercept=avg-ci,colour=paste0(parid,"-",scenario)),linetype=2)+geom_vline(data=sres[sres$parid%in%parids,],aes(xintercept=avg+ci,colour=paste0(parid,"-",scenario)),linetype=2)+
+  scale_color_discrete(name="id-scenario")+xlab(expression(bar(bw)))+stdtheme
+ggsave(filename = paste0(resdir,'distrib_nwBetweenness.png'),width=15,height=10,units='cm')
 
-sres = res %>% group_by(id,scenario) %>% summarize(parid= parid[1] ,avg = mean(moranActives),ci = sd(moranActives)*1.96/sqrt(n()))
+sres = res %>% group_by(parid,scenario) %>% summarize(avg = mean(moranActives),ci = sd(moranActives)*1.96/sqrt(n()))
 g = ggplot(res[res$parid%in%parids,],aes(x=moranActives,group=paste0(parid,"-",scenario),colour=paste0(parid,"-",scenario)))
-g+geom_density(alpha=0.4)+geom_vline(data=sres[sres$parid%in%parids,],aes(xintercept=avg,colour=paste0(parid,"-",scenario)))+geom_vline(data=sres[sres$parid%in%parids,],aes(xintercept=avg-ci,colour=paste0(parid,"-",scenario)),linetype=2)+geom_vline(data=sres[sres$parid%in%parids,],aes(xintercept=avg+ci,colour=paste0(parid,"-",scenario)),linetype=2)
+g+geom_density(alpha=0.4)+geom_vline(data=sres[sres$parid%in%parids,],aes(xintercept=avg,colour=paste0(parid,"-",scenario)))+geom_vline(data=sres[sres$parid%in%parids,],aes(xintercept=avg-ci,colour=paste0(parid,"-",scenario)),linetype=2)+geom_vline(data=sres[sres$parid%in%parids,],aes(xintercept=avg+ci,colour=paste0(parid,"-",scenario)),linetype=2)+
+  scale_color_discrete(name="id-scenario")+xlab(expression(I*"("*A*")"))+stdtheme
+ggsave(filename = paste0(resdir,'distrib_moranActives.png'),width=15,height=10,units='cm')
 
-sres = res %>% group_by(id,scenario) %>% summarize(parid= parid[1] ,avg = mean(nwRelativeLength),ci = sd(moranActives)*1.96/sqrt(n()))
+
+sres = res %>% group_by(parid,scenario) %>% summarize(avg = mean(nwRelativeLength),ci = sd(moranActives)*1.96/sqrt(n()))
 g = ggplot(res[res$parid%in%parids,],aes(x=nwRelativeLength,group=paste0(parid,"-",scenario),colour=paste0(parid,"-",scenario)))
-g+geom_density(alpha=0.4)+geom_vline(data=sres[sres$parid%in%parids,],aes(xintercept=avg,colour=paste0(parid,"-",scenario)))+geom_vline(data=sres[sres$parid%in%parids,],aes(xintercept=avg-ci,colour=paste0(parid,"-",scenario)),linetype=2)+geom_vline(data=sres[sres$parid%in%parids,],aes(xintercept=avg+ci,colour=paste0(parid,"-",scenario)),linetype=2)
+g+geom_density(alpha=0.4)+geom_vline(data=sres[sres$parid%in%parids,],aes(xintercept=avg,colour=paste0(parid,"-",scenario)))+geom_vline(data=sres[sres$parid%in%parids,],aes(xintercept=avg-ci,colour=paste0(parid,"-",scenario)),linetype=2)+geom_vline(data=sres[sres$parid%in%parids,],aes(xintercept=avg+ci,colour=paste0(parid,"-",scenario)),linetype=2)+
+  scale_color_discrete(name="id-scenario")+xlab(expression(L))+stdtheme
+ggsave(filename = paste0(resdir,'distrib_networkLength.png'),width=15,height=10,units='cm')
+
+
+###############
+## Anovas to check relative parameters influence
 
 
 m1 = lm(relativeAccessibility~betaDC+gammaCDA+gammaCDE+lambdaAcc+relocationRate+networkSpeed,data=res)
@@ -71,6 +87,27 @@ for(indic in c("relativeAccessibility","nwBetweenness","nwRelativeLength","nwRel
 
 ss = anova(m2)$`Sum Sq`;names(ss)<-rownames(anova(m2))
 100*ss / sum(ss)
+
+
+
+##
+# check distrib of distances between averages relative to confidence intervals
+sres = res %>% group_by(parid,scenario) %>% summarize(avgRelativeAccessibility = mean(relativeAccessibility),ci = sd(relativeAccessibility)*1.96/sqrt(n()))
+sres$id = paste0(sres$parid,"-",sres$scenario)
+
+#relci = c()
+#for(i in 1:(nrow(sres)-1)){
+#  show(i)
+#  for(j in (i+1):nrow(sres)){
+#    relci = append(relci,(sres$ci[i]+sres$ci[j])/abs(sres$avgRelativeAccessibility[i]-sres$avgRelativeAccessibility[j]))
+#  }
+#}
+
+relci=c((matrix(rep(sres$ci,nrow(sres)),nrow=nrow(sres),byrow = T)+matrix(rep(sres$ci,nrow(sres)),nrow=nrow(sres),byrow = F))/(abs(matrix(rep(sres$avgRelativeAccessibility,nrow(sres)),nrow=nrow(sres),byrow = T)-matrix(rep(sres$avgRelativeAccessibility,nrow(sres)),nrow=nrow(sres),byrow = F))))
+#hist(relci,breaks=1000)
+quantile(relci,0.975)
+#97.5% 
+#0.9686911 
 
 
 
