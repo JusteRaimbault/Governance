90901fad3703c6847aa82b95b01d5bc9b3022468
Juste Raimbault <juste.raimbault@polytechnique.edu>
2018-04-24 16:56:40 +0200

    corrs

--- Models/Lutecia/analysis/corrs.R
+++ Models/Lutecia/analysis/corrs.R
@@ -6,7 +6,11 @@
 timecorrs <- function(x,y,n=1,b=1){
   timesteps = length(x)/(n*b)
   #for(tw in 1:timesteps){
-    rho = cor.test(x,y)
+    # test on all possible windows of length tw
+  #  for(t0 in 1:(timesteps-tw+1)){
+  #    steps = t0:(t0+tw-1)
+      rho = cor.test(x,y)
+  #  }
   #}
   return(c(rho$estimate,rho$conf.int[1],rho$conf.int[2]))
 }
